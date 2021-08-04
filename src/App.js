cefeb7a890bd75fbbe7fe570e7c263c97bd3eca0
Gabriel Cintra <gabrieloliveiracintraa@hotmail.com>
2021-08-04 11:40:51 -0300

    feat(general): init project (04082021)

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+
+import Routes from './routes';
+import Header from './components/Header';
+
+export default function App() {
+ return (
+   <BrowserRouter>
+     <Header/>
+     <Routes/>
+   </BrowserRouter>
+ );
+}

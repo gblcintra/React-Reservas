bd225f8e4945e3fd2b290d239dd811930e00d63e
Gabriel Cintra <gabrieloliveiracintraa@hotmail.com>
2021-08-04 12:47:55 -0300

    feat(reservas): create box info (04082021)

--- src/pages/Reservas/index.js
+++ src/pages/Reservas/index.js
@@ -1,9 +1,32 @@
 import React from 'react';
+import { MdDelete } from 'react-icons/md'
+import './style.scss'
 
 export default function Reservas() {
- return (
-   <div>
-     <h1>Reservas</h1>
-   </div>
- );
+  return (
+    <div className="reservas">
+      <h1 className="title">Voce solicitou 1 reserva</h1>
+      <div className="reservas-box">
+        <img
+          src="https://sujeitoprogramador.com/wp-content/uploads/2019/12/maceio.jpg"
+          alt="Maceio"
+        ></img>
+        <strong>Viagem Maceio 7 Dias</strong>
+        <span>Quantidade: 2</span>
+        <button
+          type="button"
+          onClick={() => { }}
+        ><MdDelete size={20} color="#000" />
+        </button>
+      </div>
+
+      <footer>
+        <button
+          type="button"
+        >
+          Solicitar Reservas
+        </button>
+      </footer>
+    </div>
+  );
 }
